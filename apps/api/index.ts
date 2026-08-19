a3c4a04e771a3dbabb07631bf001402f8a119b99
Raunak <raunakkumar6437@gmail.com>
2026-08-19 13:15:29 +0530

    testing api

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -4,13 +4,20 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/test" , (req,res)=>{
+app.get("/test", (req, res) => {
     res.json({
-        message:"api end point is working boossss..."
+        message: "api end point is working boossss..."
+    })
+})
+
+app.post("/data", (req, res) => {
+    const body = req.body
+    res.json({
+        message: body
     })
 })
 
 
-app.listen(3001 , ()=>{
+app.listen(3001, () => {
     console.log(`server is running on http://localhost:3001`)
 })
