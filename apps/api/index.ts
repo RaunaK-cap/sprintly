adc0300f460f356b343e0727d3fd844af5209db6
Raunak <raunakkumar6437@gmail.com>
2026-08-29 11:48:23 +0530

    init websocket

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/userauth";
-import org_and_meb from "./routes/user-org_mem ";
+import org_and_meb from "./routes/user-org_mem";
 import boards from "./routes/user_board";
 import issues from "./routes/user_issue";
 import assignees from "./routes/user_assignee";
---
+++ apps/websocket/index.ts
@@ -0,0 +1,8 @@
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 8080 });
+
+server.on("connection", (socket) => {
+    console.log("new connection");
+    socket.send("hello");
+})
